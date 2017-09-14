0cbf2234edcbc8f7289bd1d50c710732f4fef442
Eloy Coto <eloy.coto@gmail.com>
2017-09-14 17:46:36 +0200

    Ginkgo: Firstt k8s POC test

---
+++ test/helpers/vagrant.go
@@ -0,0 +1,82 @@
+package helpers
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type Vagrant struct{}
+
+func (vagrant *Vagrant) getPath(prog string) string {
+	path, err := exec.LookPath(prog)
+	if err != nil {
+		return ""
+	}
+	return path
+}
+
+func (vagrant *Vagrant) getDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "/tmp/"
+	}
+	return fmt.Sprintf("%s/", dir)
+}
+
+func (vagrant *Vagrant) getCMD(op string) *exec.Cmd {
+	cmd := exec.Command(vagrant.getPath("bash"), "-c", op)
+	cmd.Dir = vagrant.getDir()
+	return cmd
+}
+
+func (vagrant *Vagrant) Create() error {
+
+	cmd := vagrant.getCMD("vagrant up")
+	// FIXME: output to log with proper header
+	// FIXME: Check if the VM is up, if up set reload
+	out, err := cmd.CombinedOutput()
+	fmt.Printf("%s", out)
+	if err != nil {
+		return err
+	}
+
+	err = vagrant.createConfig()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (vagrant *Vagrant) createConfig() error {
+	cmd := vagrant.getCMD("vagrant ssh-config > ssh-config")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (vagrant *Vagrant) deleteConfig() error {
+
+	cmd := vagrant.getCMD("rm ssh-config")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (vagrant *Vagrant) Destroy() error {
+
+	cmd := vagrant.getCMD("vagrant destroy -f")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	err = vagrant.deleteConfig()
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- test/k8s_test.go
+++ test/k8s_test.go
@@ -1,30 +1,107 @@
 package ciliumTest
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/cilium/cilium/test/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("K8s_basic_test", func() {
+var _ = Describe("K8sPolicyTest", func() {
 
-	kubectl := helpers.CreateKubectl("127.0.0.1", 2222)
+	kubectl := helpers.CreateKubectl("k8s1")
+	podFilter := "k8s:id=app"
+
 	BeforeEach(func() {
-		kubectl.Apply("https://raw.githubusercontent.com/eloycoto/k8s-cilium-terraform/master/scripts/rbac.yaml")
-		kubectl.Apply("https://raw.githubusercontent.com/eloycoto/k8s-cilium-terraform/master/scripts/cilium-ds.yaml")
-		kubectl.Apply("https://raw.githubusercontent.com/eloycoto/k8s-cilium-terraform/master/scripts/sample.yml")
-		kubectl.WaitforPods("kube-system", "-l k8s-app=cilium", 10)
+		kubectl.Apply("/vagrant/cilium-ds.yaml")
+		kubectl.Apply("/vagrant/demo.yaml")
+
+		status, err := kubectl.WaitforPods("kube-system", "-l k8s-app=cilium", 300)
+		Expect(status).Should(BeTrue())
+		Expect(err).Should(BeNil())
+
 	})
 
 	AfterEach(func() {
-		kubectl.Node.Execute("kubectl delete -f https://raw.githubusercontent.com/eloycoto/k8s-cilium-terraform/master/scripts/sample.yml", nil, nil)
+		// kubectl.Delete("/vagrant/cilium-ds.yaml")
 	})
 
-	Describe("Chekcing sample app is running", func() {
-		Context("Without_policy", func() {
-			It("has three pods", func() {
-				Expect(1).To(Equal(1))
-			})
+	Context("Default mode without no policy", func() {
+		It("Policy disabled", func() {
+			kubectl.Apply("/vagrant/demo.yaml")
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			//FIXME: This assert should have comment
+			Expect(err).Should(BeNil())
+			stdout, err := kubectl.CiliumExec(cilium_pod, getEndpointFilter(podFilter, "Disabled"))
+			Expect(err).Should(BeNil())
+			Expect(strings.Trim(stdout, "\n")).Should(Equal("4"))
+		}),
+
+		It("Policy enabled", func(){
+			_, err := kubectl.CiliumImportPolicy("kube-system", "/vagrant/l3_l4_policy.yaml", 300)
+			Expect(err).Should(BeNil())
+
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			//FIXME: This assert should have comment
+			Expect(err).Should(BeNil())
+
+			stdout, err := kubectl.CiliumExec(cilium_pod, getEndpointFilter(podFilter, "Disabled"))
+			Expect(err).Should(BeNil())
+			Expect(strings.Trim(stdout, "\n")).Should(Equal("2"))
+
+			stdout, err = kubectl.CiliumExec(cilium_pod, getEndpointFilter(podFilter, "Enabled"))
+			Expect(err).Should(BeNil())
+			Expect(strings.Trim(stdout, "\n")).Should(Equal("2"))
+		})
+
+		It("Policy disabled correctly", func(){
+			_,err := kubectl.Destroy("/vagrant/l3_l4_policy.yaml")
+			Expect(err).Should(BeNil())
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			Expect(err).Should(BeNil())
+			//FIXME Wait HERE
+			stdout, err := kubectl.CiliumExec(cilium_pod, getEndpointFilter(podFilter, "Disabled"))
+			Expect(err).Should(BeNil())
+			Expect(strings.Trim(stdout, "\n")).Should(Equal("4"))
+		})
+	})
+
+
+	Context("Default mode without policy enforcement", func() {
+		It("Policy enabled always", func(){
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			Expect(err).Should(BeNil())
+			_, err := kubectl.CiliumExec(cilium_pod,"cilium config PolicyEnforcement=always")
+			Expect(err).Should(BeNil())
+
+			kubectl.Apply("/vagrant/demo.yaml")
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			//FIXME: This assert should have comment
+			Expect(err).Should(BeNil())
+			stdout, err := kubectl.CiliumExec(cilium_pod, getEndpointFilter(podFilter, "Enabled"))
+			Expect(err).Should(BeNil())
+			Expect(strings.Trim(stdout, "\n")).Should(Equal("4"))
+		})
+
+		It("Policy enabled never", func(){
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			Expect(err).Should(BeNil())
+			_, err := kubectl.CiliumExec(cilium_pod,"cilium config PolicyEnforcement=never")
+			Expect(err).Should(BeNil())
+
+			kubectl.Apply("/vagrant/demo.yaml")
+			cilium_pod, err := kubectl.GetCiliumPodOnNode("kube-system", "k8s1")
+			Expect(err).Should(BeNil())
+			stdout, err := kubectl.CiliumExec(cilium_pod, getEndpointFilter(podFilter, "Disabled"))
+			Expect(err).Should(BeNil())
+			Expect(strings.Trim(stdout, "\n")).Should(Equal("4"))
 		})
 	})
 })
+
+func getEndpointFilter(filter string, status string) string {
+	return fmt.Sprintf(
+		"cilium endpoint list | grep '%s' | awk '{print $2}' | grep '%s' -c", filter, status)
+}
