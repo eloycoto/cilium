99833739eec5f0642325d652948eb4bd3f378efe
Eloy Coto <eloy.coto@gmail.com>
2017-09-14 17:46:36 +0200

    Ginkgo: Firstt k8s POC test

--- test/provision/k8s_install.sh
+++ test/provision/k8s_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 HOST=$(hostname)
+TOKEN="258062.5d84c017c9b2796c"
 CILIUM_CONFIG_DIR="/opt/cilium"
 ETCD_VERSION="v3.1.0"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+apt-get update
+apt-get install -y curl jq apt-transport-https
 
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
@@ -30,7 +34,7 @@ Description=etcd
 Documentation=https://github.com/coreos
 
 [Service]
-ExecStart=/usr/bin/etcd --name=cilium --data-dir=/var/etcd/cilium --advertise-client-urls=http://172.16.0.2:9732 --listen-client-urls=http://0.0.0.0:9732 --listen-peer-urls=http://0.0.0.0:9733
+ExecStart=/usr/bin/etcd --name=cilium --data-dir=/var/etcd/cilium --advertise-client-urls=http://192.168.36.11:9732 --listen-client-urls=http://0.0.0.0:9732 --listen-peer-urls=http://0.0.0.0:9733
 Restart=on-failure
 RestartSec=5
 
@@ -43,16 +47,25 @@ EOF
 
 sudo mount bpffs /sys/fs/bpf -t bpf
 
+if [[ "${HOST}" == "k8s1" ]]; then
+    # FIXME: IP needs to be dynamic
+    kubeadm init --token=$TOKEN --apiserver-advertise-address="192.168.36.11" --pod-network-cidr=10.10.0.0/16
 
-kubeadm init
-mkdir -p /root/.kube
-sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
-sudo chown root:root /root/.kube/config
+    mkdir -p /root/.kube
+    sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
+    sudo chown root:root /root/.kube/config
 
-sudo -u ubuntu mkdir -p /home/ubuntu/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
+    sudo -u vagrant mkdir -p /home/vagrant/.kube
+    sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+    sudo chown vagrant:vagrant /home/vagrant/.kube/config
 
-sudo cp /etc/kubernetes/admin.conf ${CILIUM_CONFIG_DIR}/kubeconfig
-kubectl taint nodes --all node-role.kubernetes.io/master-
+    sudo cp /etc/kubernetes/admin.conf ${CILIUM_CONFIG_DIR}/kubeconfig
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+
+    install_etcd
+else
+    kubeadm join --token=$TOKEN 192.168.36.11:6443
+    cp /etc/kubernetes/kubelet.conf ${CILIUM_CONFIG_DIR}/kubeconfig
+fi
+
 # install_etcd
