0e8f667fa291eeb273d6f676d6a1e9810c2a6824
Eloy Coto <eloy.coto@gmail.com>
2017-09-21 11:02:04 +0200

    WIP

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -3,6 +3,7 @@ package k8sT
 import (
 	"fmt"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/cilium/cilium/test/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -15,6 +16,7 @@ var _ = Describe("K8sTunnelTest", func() {
 	var kubectl *helpers.Kubectl
 	var logger *log.Entry
 	var initilized bool
+	var serviceName string = "app1-service"
 
 	initilize := func() {
 		if initilized == true {
@@ -24,17 +26,35 @@ var _ = Describe("K8sTunnelTest", func() {
 		logger.Info("Starting")
 
 		kubectl = helpers.CreateKubectl("k8s1", logger)
+		path := fmt.Sprintf("%s/cilium_ds.yaml", kubectl.ManifestsPath())
+		kubectl.Apply(path)
+		_, err := kubectl.WaitforPods("kube-system", "-l k8s-app=cilium", 300)
+		Expect(err).Should(BeNil())
 		initilized = true
 	}
 
 	BeforeEach(func() {
 		initilize()
-	})
-	// demoDSPath := fmt.Sprintf("%s/demo_ds.yaml", kubectl.ManifestsPath())
+		demoDSPath := fmt.Sprintf("%s/demo.yaml", kubectl.ManifestsPath())
+		kubectl.Apply(demoDSPath)
+		status, err := kubectl.WaitforPods("default", "-l zgroup=testapp", 300)
+		Expect(status).Should(BeTrue())
+		Expect(err).Should(BeNil())
 
-	FIt("Check Service", func() {
-		fmt.Println(kubectl)
-		Expect(nil).Should(BeNil())
 	})
 
+	It("Check Service", func() {
+		svcIP, err := kubectl.Get(
+			"default", fmt.Sprintf("service %s", serviceName)).Filter("{.spec.clusterIP}")
+		Expect(err).Should(BeNil())
+		Expect(govalidator.IsIP(svcIP.String())).Should(BeTrue())
+
+		status := kubectl.Node.Execute(fmt.Sprintf("curl http://%s/", svcIP), nil, nil)
+		Expect(status).Should(BeTrue())
+
+		//FIXME: Validate here that the cilium service list -o jsonpath='{.svc.IP:80}"'
+	})
+
+	//FIXME: Check service with IPV6
+	//FIXME: Check the service with cross-node
 })
