b4aea4ffb17d364908a4aae94e4a4fe1b701f474
Eloy Coto <eloy.coto@gmail.com>
2017-09-22 09:13:45 +0200

    Refactor Vagrant helper

--- test/helpers/vagrant.go
+++ test/helpers/vagrant.go
@@ -4,38 +4,24 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 //Vagrant helper struct
 type Vagrant struct{}
 
-func (vagrant *Vagrant) getPath(prog string) string {
-	path, err := exec.LookPath(prog)
-	if err != nil {
-		return ""
-	}
-	return path
-}
-
-func (vagrant *Vagrant) getDir() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		return "/tmp/"
-	}
-	return fmt.Sprintf("%s/", dir)
-}
-
-func (vagrant *Vagrant) getCMD(op string) *exec.Cmd {
-	cmd := exec.Command(vagrant.getPath("bash"), "-c", op)
-	cmd.Dir = vagrant.getDir()
-	return cmd
-}
-
 //Create a new vagrant server
 func (vagrant *Vagrant) Create() error {
-	cmd := vagrant.getCMD("vagrant up")
+	createCMD := "vagrant up"
+
+	for _, v := range vagrant.Status() {
+		if v == "running" {
+			createCMD = "vagrant provision"
+			break
+		}
+	}
+	cmd := vagrant.getCMD(createCMD)
 	// FIXME: output to log with proper header
-	// FIXME: Check if the VM is up, if up set reload
 	out, err := cmd.CombinedOutput()
 	fmt.Printf("%s", out)
 	if err != nil {
@@ -81,4 +67,44 @@ func (vagrant *Vagrant) Destroy() error {
 		return err
 	}
 	return nil
+}
+
+func (vagrant *Vagrant) getCMD(op string) *exec.Cmd {
+	cmd := exec.Command(vagrant.getPath("bash"), "-c", op)
+	cmd.Dir = vagrant.getDir()
+	return cmd
+}
+
+func (vagrant *Vagrant) getDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "/tmp/"
+	}
+	return fmt.Sprintf("%s/", dir)
+}
+
+func (vagrant *Vagrant) getPath(prog string) string {
+	path, err := exec.LookPath(prog)
+	if err != nil {
+		return ""
+	}
+	return path
+}
+
+//Status return a map with the server name (key) and the status as value
+func (vagrant *Vagrant) Status(key ...string) map[string]string {
+	var result map[string]string = map[string]string{}
+
+	cmd := vagrant.getCMD("vagrant status --machine-readable")
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return result
+	}
+	for _, line := range strings.Split(string(data), "\n") {
+		val := strings.Split(line, ",")
+		if len(val) > 2 && val[2] == "state" {
+			result[val[1]] = val[3]
+		}
+	}
+	return result
 }
