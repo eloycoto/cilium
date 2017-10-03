8a1a56d024b35899f1ab03e6a814a70ab3f7dcef
Eloy Coto <eloy.coto@gmail.com>
2017-10-03 16:27:56 +0200

    Added policyEnforcement tests

--- test/helpers/utils.go
+++ test/helpers/utils.go
@@ -5,3 +5,16 @@ import "time"
 func Sleep(delay time.Duration) {
 	time.Sleep(delay * time.Second)
 }
+
+//CountValues: Filter an array of strings and return the number of matches and
+//the len of the array
+func CountValues(key string, data []string) (int, int) {
+	var result int
+
+	for _, x := range data {
+		if x == key {
+			result++
+		}
+	}
+	return result, len(data)
+}
