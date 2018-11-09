8e29b47d055a3f64ede49fed1b00514641e52edb
Eloy Coto <eloy.coto@gmail.com>
2018-11-09 15:39:01 +0100

    Policy: Added ToGroups option into Egress policy.
    To be able to add new integrations, a new field ToGroups was added.
    These information will be saved in the parent rule, and if the ToGroups
    is in place it'll return a new CIDR rule that will get the information
    from the provider callbacks.
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- pkg/k8s/apis/cilium.io/v2/types.go
+++ pkg/k8s/apis/cilium.io/v2/types.go
@@ -67,6 +67,9 @@ type CiliumNetworkPolicy struct {
 type CiliumNetworkPolicyStatus struct {
 	// Nodes is the Cilium policy status for each node
 	Nodes map[string]CiliumNetworkPolicyNodeStatus `json:"nodes,omitempty"`
+
+	// ChildrenPolicies is the Cilium Children policies status
+	ChildrenPolicies map[string]CiliumChildPolicyStatus `json:"childrenPolicies,omitempty"`
 }
 
 // CiliumNetworkPolicyNodeStatus is the status of a Cilium policy rule for a
@@ -99,6 +102,21 @@ type CiliumNetworkPolicyNodeStatus struct {
 	// CNP that was imported corresponding to Annotation X=Y has been realized on
 	// the node.
 	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+type CiliumChildPolicyStatus struct {
+	// OK is true when the policy has been parsed and imported successfully
+	// into the in-memory policy repository on the node.
+	OK bool `json:"ok,omitempty"`
+	// Error describes any error that occurred when parsing or importing the
+	// policy, or realizing the policy for the endpoints to which it applies
+	// on the node.
+	Error string `json:"error,omitempty"`
+	// LastUpdated contains the last time this status was updated
+	LastUpdated Timestamp `json:"lastUpdated,omitempty"`
+	// Enforcing is set to true once all endpoints present at the time the
+	// policy has been imported are enforcing this policy.
+	Enforcing bool `json:"enforcing,omitempty"`
 }
 
 // NewTimestamp creates a new Timestamp with the current time.Now()
@@ -149,6 +167,14 @@ func (r *CiliumNetworkPolicy) SetPolicyStatus(nodeName string, cnpns CiliumNetwo
 		r.Status.Nodes = map[string]CiliumNetworkPolicyNodeStatus{}
 	}
 	r.Status.Nodes[nodeName] = cnpns
+}
+
+// SetChildrenPolicy set the status for the given children policy.
+func (r *CiliumNetworkPolicy) SetChildrenPolicy(name string, status CiliumChildPolicyStatus) {
+	if r.Status.ChildrenPolicies == nil {
+		r.Status.ChildrenPolicies = map[string]CiliumChildPolicyStatus{}
+	}
+	r.Status.ChildrenPolicies[name] = status
 }
 
 // SpecEquals returns true if the spec and specs metadata is the sa
@@ -218,6 +244,34 @@ func (r *CiliumNetworkPolicy) GetIdentityLabels() labels.LabelArray {
 	uid := r.ObjectMeta.UID
 	return k8sCiliumUtils.GetPolicyLabels(namespace, name, uid,
 		k8sCiliumUtils.ResourceTypeCiliumNetworkPolicy)
+}
+
+func (r *CiliumNetworkPolicy) HasChildrenPolicies() bool {
+	if r.Spec != nil {
+		if r.Spec.HasChildRule() {
+			return true
+		}
+	}
+	if r.Specs != nil {
+		for _, rule := range r.Specs {
+			if rule.HasChildRule() {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (r *CiliumNetworkPolicy) ChildrenPoliciesAlreadyEnforce() bool {
+	if !r.HasChildrenPolicies() {
+		return true
+	}
+	for _, status := range r.Status.ChildrenPolicies {
+		if !status.Enforcing {
+			return false
+		}
+	}
+	return true
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
